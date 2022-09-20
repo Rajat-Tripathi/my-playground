275473ef7bf87a69a753089b69389c8a9d6e376b
Rajath <rajath.t@reactore.com>
2022-09-20 18:16:37 +0530

    modified Regulator

--- src/main/scala/others/Regulator.scala
+++ src/main/scala/others/Regulator.scala
@@ -1,5 +1,7 @@
 package others
 
+import java.util.concurrent.atomic.AtomicReference
+
 import org.joda.time.{DateTime, Seconds}
 
 /**
@@ -8,53 +10,62 @@ import org.joda.time.{DateTime, Seconds}
 
 class Regulator(maxPerSecond: Int, maxPerDay: Int) {
 
-  private var invoke: Invoke = Invoke(0, 0, DateTime.now())
+  private val counterRef: AtomicReference[Counter] = {
+    new AtomicReference[Counter](Counter(0, 0, DateTime.now()))
+  }
 
-  private def checkLimits: Either[String, Boolean] = {
-    if (invoke.totalAttempt < maxPerDay) {
-      val sec = invoke.getSeconds
-      if (sec <= 1 && invoke.perSecAttempt < maxPerSecond) {
-        println("here")
-        invoke = invoke.incrementNow()
+  /** Checks for quota */
+  private def checkForQuota: Either[String, Boolean] = {
+    val counter = counterRef.get()
+    if (counter.totalAttempt < maxPerDay) {
+      val sec = counter.getSeconds
+      if (sec <= 1 && counter.perSecAttempt < maxPerSecond) {
+        counterRef.set(counter.incrementNow())
         Right(true)
       } else if (sec <= 1) {
-        Left("Max per second quota crossed !!")
+        Left("Max attempts per second quota exceeded !!")
       } else {
-        invoke = invoke.resetPerSec()
+        counterRef.set(counter.resetPerSecAttempt())
         Right(true)
       }
     } else {
-      if (invoke.hasDayPassed) {
-        invoke = invoke.resetPerDay()
+      if (counter.hasDayPassed) {
+        counterRef.set(counter.resetPerDayAttempt())
         Right(true)
       } else {
-        Left("Max per day quota crossed !!")
+        Left("Max attempts per day quota exceeded !!")
       }
     }
   }
 
+  /** Regulates the invocation based on quota */
   def regulate[O](fn: => O): O = {
-    if (checkLimits.isRight) {
+    if (checkForQuota.isRight) {
       fn
     } else {
-      throw new Exception(checkLimits.left.get)
+      throw new Exception(checkForQuota.left.get)
     }
   }
 
 }
 
-sealed case class Invoke(perSecAttempt: Int, totalAttempt: Int, timeStamp: DateTime) {
+sealed case class Counter(perSecAttempt: Int, totalAttempt: Int, timeStamp: DateTime) {
 
-  def incrementNow(): Invoke = Invoke(this.perSecAttempt + 1, this.totalAttempt + 1, DateTime.now())
+  /** Increment attempts count */
+  def incrementNow(): Counter = Counter(this.perSecAttempt + 1, this.totalAttempt + 1, DateTime.now())
 
-  def resetPerSec(): Invoke = Invoke(1, this.totalAttempt + 1, DateTime.now())
+  /** Resets total attempts count for the second for the next second */
+  def resetPerSecAttempt(): Counter = Counter(1, this.totalAttempt + 1, DateTime.now())
 
-  def resetPerDay(): Invoke = Invoke(this.perSecAttempt + 1, 1, DateTime.now())
+  /** Resets total attempts count for the day for the next day */
+  def resetPerDayAttempt(): Counter = Counter(this.perSecAttempt + 1, 1, DateTime.now())
 
+  /** Gets seconds between last attempt timeStamp and current timestamp */
   def getSeconds: Int = {
     Seconds.secondsBetween(this.timeStamp, DateTime.now()).getSeconds
   }
 
+  /** Checks if midnight passed */
   def hasDayPassed: Boolean = {
     this.timeStamp.plusDays(1).withTimeAtStartOfDay().getMillis <= DateTime.now().getMillis
   }
@@ -75,7 +86,7 @@ object CheckRegulator extends App {
   }
 
   (1 to 10).foreach{ x =>
-    Thread.sleep(2000)
+    //    Thread.sleep(2000)
     callEmailService()
   }
 
